0f5bb550c4b71a946703091c7a15861973663659
Alan Smith <a.bax.smith@gmail.com>
2014-10-01 14:55:51 -0600

    guests can view individual programs

---
+++ spec/factories.rb
@@ -0,0 +1,13 @@
+FactoryGirl.define do
+  factory :program do
+    facility_name   "Catholic Charities"
+    street_address1 "4045 N Pecos St"
+    street_address2 nil
+    city            "Denver"
+    state           "CO"
+    zipcode         "80211"
+    phone           "303-742-0828"
+    program_type    "Administrative Offices"
+    enrollment      0
+  end
+end
---
+++ spec/features/program_show_page_spec.rb
@@ -0,0 +1,17 @@
+require "feature_helper"
+
+describe "the program show view", type: :feature do
+
+  before(:each) do
+    @program = FactoryGirl.create(:program)
+  end
+
+  it "as a guest I can visit an individual program page" do
+    visit programs_path
+    click_link(@program.facility_name)
+
+    expect(page).to have_content(@program.facility_name)
+    expect(current_path).to eq(program_path(@program))
+  end
+end
+
