b36e51af84dc0ac7952be99580e7102e66297b5f
Alan Smith <a.bax.smith@gmail.com>
2014-10-09 13:30:44 -0600

    fixed tests

--- spec/features/favorites_spec.rb
+++ spec/features/favorites_spec.rb
@@ -10,9 +10,9 @@ describe "viewing favorites", type: :feature do
     visit programs_path
     click_link("View")
     click_button("Add to Favorites")
-
-    expect(current_path).to eq(favorites_path)
+  
     expect(page).to have_content("#{@program.facility_name} has been added to your favorites")
+    visit favorites_path
     expect(page).to have_content("#{@program.facility_name}")
   end
 end
